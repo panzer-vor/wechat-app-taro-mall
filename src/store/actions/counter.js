46656922c8216febefea596b46bcc2873bf0ddea
unknown <you@example.com>
2019-08-11 13:37:33 +0800

    完成在线客服

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -1,5 +1,4 @@
-import { map } from 'rxjs/operators'
-import { obsGet } from 'utils/obs_request'
+import { get } from 'utils/request'
 import {
   ADD,
   MINUS,
@@ -19,19 +18,31 @@ export const asyncAdd = () => dispatch => setTimeout(() => {
     dispatch(add())
   }, 2000)
 
-export const asyncList = (studentId) => 
-  dispatch => {
-    obsGet({
-      uri: 'course-arrange',
-      data: {
-        studentId
-      }
-    }).pipe(
-      map(v => v)
-    ).subscribe(v =>
-      dispatch({
-        type: ASYNC_LIST,
-        payload: v
-      })
-    )
-  }
+// export const asyncList = (studentId) => dispatch => 
+//   obsGet({
+//     uri: 'course-arrange',
+//     data: {
+//       studentId
+//     }
+//   }).pipe(
+//     map(v => v)
+//   ).subscribe(v =>
+//     dispatch({
+//       type: ASYNC_LIST,
+//       payload: v
+//     })
+//   )
+
+export const asyncList = (studentId) => dispatch => 
+  get({
+    uri: 'course-arrange',
+    data: {
+      studentId
+    }
+  })
+  .then(res => dispatch({
+    type: ASYNC_LIST,
+    payload: res
+  }))
+    
+  
